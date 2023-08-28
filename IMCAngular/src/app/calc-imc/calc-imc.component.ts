ca19b1f15ef27e34b110ea3286960564c68d6f9d
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-08-28 14:54:31 -0300

    Bug fix

--- IMCAngular/src/app/calc-imc/calc-imc.component.ts
+++ IMCAngular/src/app/calc-imc/calc-imc.component.ts
@@ -9,9 +9,18 @@ export class CalcIMCComponent {
   peso: number=0;
   altura: number=0;
   resultado: number=0;
+  texto: string="Resultado:";
 
   IMC()
   {
-    this.resultado = this.peso/(this.altura*this.altura);
+    if(this.altura == 0)
+    {
+      this.resultado = 0;
+    }
+    else
+    {
+      this.resultado = this.peso/(this.altura*this.altura);
+    }
   }
 }
+
